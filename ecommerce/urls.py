014789a5271f01b8567f4654e1ec512a50be2f16
dise97 <sehovicadis97@hotmail.com>
2021-06-07 00:57:10 +0200

    second commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('', include('store.urls')),
     path('', include('django.contrib.auth.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
