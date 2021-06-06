6bccb96da2e01ac1be1016ca98a959fe0cf8741d
dise97 <sehovicadis97@hotmail.com>
2021-06-07 01:36:35 +0200

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,10 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
 MEDIA_URL='/images/'
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
