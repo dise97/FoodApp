805d26dc5dfba455607c7edde08334499148c29c
dise97 <sehovicadis97@hotmail.com>
2021-06-06 22:48:08 +0200

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!j-#xjyxrjpkv=au*+7^%jxh_n)uh=_)ow+(pq8hx1p*%szj*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
