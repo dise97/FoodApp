eca5a177dca0e188afbd24d65f001522c28fd7e7
dise97 <sehovicadis97@hotmail.com>
2021-06-07 00:48:16 +0200

    commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,11 +20,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 from register import views as v
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.register, name="register"),
     path('', include('store.urls')),
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
