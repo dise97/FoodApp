e803b56f9a2bbb1b27bdd8cd575694717147d327
dise97 <sehovicadis97@hotmail.com>
2021-06-07 09:46:31 +0200

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'store.apps.StoreConfig',
     'register.apps.RegisterConfig',
-    
 ]
 
 MIDDLEWARE = [
@@ -123,12 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL='/images/'
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
